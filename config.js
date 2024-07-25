7e7b4eccf1ebbeb37276c6b162b56cdb4ba541da
rafael12mt <160365955+rafael12mt@users.noreply.github.com>
2024-07-25 12:09:04 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAioRtryd6TAbpPN9nGRuOBt6S5elWh5rs",
+  authDomain: "upgradeapp-e2a62.firebaseapp.com",
+  databaseURL: "https://upgradeapp-e2a62-default-rtdb.firebaseio.com",
+  projectId: "upgradeapp-e2a62",
+  storageBucket: "upgradeapp-e2a62.appspot.com",
+  messagingSenderId: "1045768019442",
+  appId: "1:1045768019442:web:77151978e85c338c2fdf6b",
+  measurementId: "G-REL2TTMNR6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
